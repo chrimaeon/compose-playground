65957e84be48dd2d535fcce11f0eadee04bf495d
Christian Grach <christian.grach@cmgapps.com>
2024-09-22 18:20:23 +0200

    add loading indicator

--- app/src/main/java/com/cmgapps/android/compose/screen/molecule/MoleculeScreen.kt
+++ app/src/main/java/com/cmgapps/android/compose/screen/molecule/MoleculeScreen.kt
@@ -6,13 +6,25 @@
 
 package com.cmgapps.android.compose.screen.molecule
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -28,12 +40,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
 import com.cmgapps.android.compose.R
 import com.cmgapps.android.compose.viewmodel.PupperPicsViewModel
 
@@ -54,7 +69,7 @@ fun MoleculeScreen(
         },
     ) { innerPadding ->
 
-        val models by viewModel.models.collectAsStateWithLifecycle()
+        val model by viewModel.models.collectAsStateWithLifecycle()
         var expanded by remember { mutableStateOf(false) }
 
         Column(
@@ -64,59 +79,120 @@ fun MoleculeScreen(
                     .padding(16.dp)
                     .fillMaxSize(),
         ) {
-            ExposedDropdownMenuBox(
-                modifier = Modifier.fillMaxWidth(),
-                expanded = expanded,
-                onExpandedChange = {
-                    expanded = it
-                },
-            ) {
-                TextField(
-                    modifier =
-                        Modifier
-                            .fillMaxWidth()
-                            .menuAnchor(MenuAnchorType.PrimaryNotEditable),
-                    value = models.dropdownText ?: stringResource(R.string.select_breed),
-                    onValueChange = {},
-                    readOnly = true,
-                    singleLine = true,
-                    label = { Text(stringResource(R.string.dog_breed)) },
-                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
-                    colors = ExposedDropdownMenuDefaults.textFieldColors(),
-                )
-                ExposedDropdownMenu(
+            AnimatedVisibility(!model.loading) {
+                ExposedDropdownMenuBox(
+                    modifier = Modifier.fillMaxWidth(),
                     expanded = expanded,
-                    onDismissRequest = { expanded = false },
+                    onExpandedChange = {
+                        expanded = it
+                    },
                 ) {
-                    models.breeds.forEach { breed ->
-                        DropdownMenuItem(
-                            text = { Text(breed.name, style = MaterialTheme.typography.bodyLarge) },
-                            onClick = {
-                                viewModel.take(Event.SelectBreed(breed))
-                                expanded = false
-                            },
-                            contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
-                        )
+                    TextField(
+                        modifier =
+                            Modifier
+                                .fillMaxWidth()
+                                .menuAnchor(MenuAnchorType.PrimaryNotEditable),
+                        value = model.dropdownText ?: stringResource(R.string.select_breed),
+                        onValueChange = {},
+                        readOnly = true,
+                        singleLine = true,
+                        label = { Text(stringResource(R.string.dog_breed)) },
+                        trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                        colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                    )
+                    ExposedDropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false },
+                    ) {
+                        model.breeds.forEach { breed ->
+                            DropdownMenuItem(
+                                text = { Text(breed.name, style = MaterialTheme.typography.bodyLarge) },
+                                onClick = {
+                                    viewModel.take(Event.SelectBreed(breed))
+                                    expanded = false
+                                },
+                                contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                            )
+                        }
                     }
                 }
             }
 
             Spacer(Modifier.height(16.dp))
 
-            AsyncImage(
-                modifier =
-                    Modifier.fillMaxSize().clickable(
+            Content(
+                modifier = Modifier.weight(1f),
+                model = model,
+                onClick = { viewModel.take(Event.FetchAgain) },
+            )
+        }
+    }
+}
+
+@Composable
+private fun Content(
+    model: Model,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+    var imageLoading by remember { mutableStateOf(true) }
+
+    Box(
+        modifier = modifier,
+    ) {
+        AsyncImage(
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .clickable(
                         onClickLabel =
                             stringResource(
                                 R.string.load_another_image,
-                                models.dropdownText ?: "",
+                                model.dropdownText ?: "",
                             ),
-                    ) {
-                        viewModel.take(Event.FetchAgain)
-                    },
-                model = models.currentUrl,
-                contentDescription = stringResource(R.string.content_description_dog_image, models.dropdownText ?: ""),
+                        onClick = onClick,
+                    ),
+            model = model.currentUrl,
+            onState = { imageLoading = it !is AsyncImagePainter.State.Success },
+            contentDescription = stringResource(R.string.content_description_dog_image, model.dropdownText ?: ""),
+        )
+        AnimatedVisibility(
+            model.loading || imageLoading,
+            enter = fadeIn(),
+            exit = fadeOut(),
+        ) {
+            Loading(
+                Modifier
+                    .fillMaxSize()
+                    .background(color = MaterialTheme.colorScheme.background),
             )
         }
+    }
+}
+
+@Composable
+private fun Loading(modifier: Modifier = Modifier) {
+    val rotation by rememberInfiniteTransition("loading animation").animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(tween(durationMillis = 1_000)),
+        label = "loading rotation",
+    )
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxSize(),
+    ) {
+        Text(
+            text = "🐶",
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier.rotate(rotation),
+        )
+        Spacer(modifier = Modifier.size(8.dp))
+        Text(
+            text = stringResource(R.string.loading),
+            style = MaterialTheme.typography.headlineSmall,
+        )
     }
 }
