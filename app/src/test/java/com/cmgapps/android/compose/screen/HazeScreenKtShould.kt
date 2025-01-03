9ca2fa2ccef9d205f1398ada8dc126327e82b2b2
Christian Grach <christian.grach@cmgapps.com>
2025-01-03 18:21:03 +0100

    update dependencies

--- app/src/test/java/com/cmgapps/android/compose/screen/HazeScreenKtShould.kt
+++ app/src/test/java/com/cmgapps/android/compose/screen/HazeScreenKtShould.kt
@@ -1,12 +1,10 @@
 package com.cmgapps.android.compose.screen
 
 import com.cmgapps.android.compose.test.PaparazziTest
-import org.junit.Ignore
 import org.junit.Test
 import kotlin.random.Random
 
 class HazeScreenKtShould : PaparazziTest() {
-    @Ignore("Software rendering doesn't support drawRenderNode")
     @Test
     fun `render haze screen`() {
         paparazzi.snapshot {
